d37177d26f3c179a7decbe3f0f0c3b99502a0b29
Paul <limseunghyun90@gmail.com>
2020-01-14 15:31:53 +0900

    서비스 수정 / 포트폴리오 내용 수정

--- pages/service/index.vue
+++ pages/service/index.vue
@@ -3,11 +3,11 @@
     <div class="container">
       <!-- 서비스 제공 가치 -->
       <div class="service__value service__common">
-        <h1>01. what we do?</h1>
+        <h1>1. what we do?</h1>
         <div class="common__contents">
           <p class="contents__text">
             피벗은 디자인과 개발 팀으로 이루어져 있습니다.<br>
-            트렌드에 맞는 기술을 활용하지만 필요에 따라 다른 기술도 활용합니다.<br>
+            트렌드에 맞는 기술을 활용하지만 필요에 따라 다른 기술도 활용합니다.<br class="mobile-hidden">
             솔루션과 CMS 기반으로 개발 가능합니다.
           </p>
           <ul class="contents__list">
@@ -23,10 +23,10 @@
 
       <!-- 우리가 하지 않는 것 -->
       <div class="service__not service__common">
-        <h1>02. we don't do!</h1>
+        <h1>2. we don't do!</h1>
         <div class="common__contents">
           <p class="contents__text">
-            저희의 시간을 절약하기 위해 “하지 않는 것” 목록을 작성하였습니다.<br>
+            저희의 시간 절약과 웹사이트 품질을 위해 “하지 않는 것” 목록을 작성하였습니다.<br class="mobile-hidden">
             프로젝트 수주를 위해 거짓을 말하지 않습니다.
           </p>
           <ul class="contents__list">
@@ -41,143 +41,108 @@
 
       <!-- 서비스 프로세스 -->
       <div class="service__method service__common">
-        <h1>03. design method</h1>
+        <h1>3. design method</h1>
         <p class="contents__text">
           저희는 7가지 단계를 거쳐 웹사이트를 만듭니다.<br>
-          사업의 방향에 맞춰 지속 성장이 가능한 웹사이트를 만들기 위해 고민을 거듭합니다.<br>
-          한 번 만드는 웹사이트가 만족할 수 있도록 최선을 다합니다.
+          사업의 방향에 맞춰 지속 성장이 가능한 웹사이트를 만들기 위해 고민을 거듭합니다.<br class="mobile-hidden">
+          한 번 만드는 웹사이트를 만족할 수 있도록 최선을 다합니다.
         </p>
         <ul class="service__process">
-          <li class="row">
-            <div class="col-6">
-              <figure class="service__thumbnail service__thumbnail01"></figure>
-            </div>
-            <div class="col-6">
-              <div class="service__contents">
-                <div class="service__header">
-                  <span class="number">01</span>
-                  <h3 class="title">사전미팅</h3>
-                  <span class="hash">#Clishe</span>
-                  <span class="hash">#Needs</span>
-                  <span class="hash">#Contract</span>
-                </div>
-                <p class="service__wording">
-                  클라이언트의 요구사항과 사용자의 니즈를 파악하는 것이 가장 중요하다는 것을 알고 있습니다.
-                  때문에 본격적인 작업 전, 제작 의도를 명확히 판단하기 위해 사전 미팅은 무조건 1회 이상 진행합니다.
-                </p>
+          <li>
+            <div class="service__contents">
+              <div class="service__header">
+                <span class="number">01</span>
+                <h3 class="title">사전미팅</h3>
+                <span class="hash">#Clishe</span>
+                <span class="hash">#Needs</span>
+                <span class="hash">#Contract</span>
               </div>
+              <p class="service__wording">
+                클라이언트의 요구사항과 사용자의 니즈를 파악하는 것이 가장 중요하다는 것을 알고 있습니다.
+                때문에 본격적인 작업 전, 제작 의도를 명확히 판단하기 위해 사전 미팅은 무조건 1회 이상 진행합니다.
+              </p>
             </div>
           </li>
-          <li class="row flex-reverse">
-            <div class="col-6">
-              <div class="service__contents">
-                <div class="service__header">
-                  <span class="number">02</span>
-                  <h3 class="title">정보수집</h3>
-                  <span class="hash">#Concept</span>
-                  <span class="hash">#Reference</span>
-                  <span class="hash">#Research</span>
-                </div>
-                <p class="service__wording">
-                  사전 미팅에서 얻은 이해를 바탕으로 클라이언트가 의도한 컨셉과 정의를 디자인에 접목시킬 수 있도록 필요한 정보를 수집합니다. 클라이언트가 제공해야 하는 정보들은 별도로 요청합니다.
-                </p>
+          <li>
+            <div class="service__contents">
+              <div class="service__header">
+                <span class="number">02</span>
+                <h3 class="title">정보수집</h3>
+                <span class="hash">#Concept</span>
+                <span class="hash">#Reference</span>
+                <span class="hash">#Research</span>
               </div>
-            </div>
-            <div class="col-6">
-              <figure class="service__thumbnail service__thumbnail02"></figure>
+              <p class="service__wording">
+                사전 미팅에서 얻은 이해를 바탕으로 클라이언트가 의도한 컨셉과 정의를 디자인에 접목시킬 수 있도록 필요한 정보를 수집합니다. 클라이언트가 제공해야 하는 정보들은 별도로 요청합니다.
+              </p>
             </div>
           </li>
-          <li class="row">
-            <div class="col-6">
-              <figure class="service__thumbnail service__thumbnail04"></figure>
-            </div>
-            <div class="col-6">
-              <div class="service__contents">
-                <div class="service__header">
-                  <span class="number">03</span>
-                  <h3 class="title">전략수립</h3>
-                  <span class="hash">#Schedule</span>
-                  <span class="hash">#Process</span>
-                </div>
-                <p class="service__wording">
-                  주요 목표를 달성할 수 있도록 전략을 수립합니다. 이 단계에서 일정표와 기능정의서 등 필요한 문서들을 작성하여 클라이언트에게 공유합니다.
-                </p>
+          <li>
+            <div class="service__contents">
+              <div class="service__header">
+                <span class="number">03</span>
+                <h3 class="title">전략수립</h3>
+                <span class="hash">#Schedule</span>
+                <span class="hash">#Process</span>
               </div>
+              <p class="service__wording">
+                주요 목표를 달성할 수 있도록 전략을 수립합니다. 이 단계에서 일정표와 기능정의서 등 필요한 문서들을 작성하여 클라이언트에게 공유합니다.
+              </p>
             </div>
           </li>
-          <li class="row flex-reverse">
-            <div class="col-6">
-              <div class="service__contents">
-                <div class="service__header">
-                  <span class="number">04</span>
-                  <h3 class="title">와이어프레임</h3>
-                  <span class="hash">#Sample</span>
-                  <span class="hash">#Opinion</span>
-                  <span class="hash">#Communication</span>
-                </div>
-                <p class="service__wording">
-                  의도한 컨셉과 정의를 시각적으로 확인할 수 있도록 임시 콘텐츠를 만듭니다. 전체적인 기틀이 만들어지는 중요한 단계이므로 적극적인 소통과 의견 교환이 필요합니다.
-                </p>
+          <li>
+            <div class="service__contents">
+              <div class="service__header">
+                <span class="number">04</span>
+                <h3 class="title">와이어프레임</h3>
+                <span class="hash">#Sample</span>
+                <span class="hash">#Opinion</span>
+                <span class="hash">#Communication</span>
               </div>
-            </div>
-            <div class="col-6">
-              <figure class="service__thumbnail service__thumbnail03"></figure>
+              <p class="service__wording">
+                의도한 컨셉과 정의를 시각적으로 확인할 수 있도록 임시 콘텐츠를 만듭니다. 전체적인 기틀이 만들어지는 중요한 단계이므로 적극적인 소통과 의견 교환이 필요합니다.
+              </p>
             </div>
           </li>
-          <li class="row">
-            <div class="col-6">
-              <figure class="service__thumbnail service__thumbnail05"></figure>
-            </div>
-            <div class="col-6">
-              <div class="service__contents">
-                <div class="service__header">
-                  <span class="number">05</span>
-                  <h3 class="title">디자인</h3>
-                  <span class="hash">#UX/UI</span>
-                  <span class="hash">#Visual</span>
-                </div>
-                <p class="service__wording">
-                  임시 콘텐츠를 토대로 시각적으로 표현합니다. 많은 고민과 시간이 들어가는 단계이므로 최소한의 수정사항만을 반영합니다.
-                </p>
+          <li>
+            <div class="service__contents">
+              <div class="service__header">
+                <span class="number">05</span>
+                <h3 class="title">디자인</h3>
+                <span class="hash">#UX/UI</span>
+                <span class="hash">#Visual</span>
               </div>
+              <p class="service__wording">
+                임시 콘텐츠를 토대로 시각적으로 표현합니다. 많은 고민과 시간이 들어가는 단계이므로 최소한의 수정사항만을 반영합니다.
+              </p>
             </div>
           </li>
-          <li class="row flex-reverse">
-            <div class="col-6">
-              <div class="service__contents">
-                <div class="service__header">
-                  <span class="number">06</span>
-                  <h3 class="title">개발</h3>
-                  <span class="hash">#supplement</span>
-                  <span class="hash">#Function</span>
-                  <span class="hash">#UX/UI</span>
-                </div>
-                <p class="service__wording">
-                  디자인이 완성되면 퍼블리싱 및 개발을 진행합니다. 필요하다면 임시 서버를 공유하여 제작 상황을 확인하실 수 있습니다.
-                </p>
+          <li>
+            <div class="service__contents">
+              <div class="service__header">
+                <span class="number">06</span>
+                <h3 class="title">개발</h3>
+                <span class="hash">#supplement</span>
+                <span class="hash">#Function</span>
+                <span class="hash">#UX/UI</span>
               </div>
-            </div>
-            <div class="col-6">
-              <figure class="service__thumbnail service__thumbnail06"></figure>
+              <p class="service__wording">
+                디자인이 완성되면 퍼블리싱 및 개발을 진행합니다. 필요하다면 임시 서버를 공유하여 제작 상황을 확인하실 수 있습니다.
+              </p>
             </div>
           </li>
-          <li class="row">
-            <div class="col-6">
-              <figure class="service__thumbnail service__thumbnail07"></figure>
-            </div>
-            <div class="col-6">
-              <div class="service__contents">
-                <div class="service__header">
-                  <span class="number">07</span>
-                  <h3 class="title">기타 서비스</h3>
-                  <span class="hash">#Operation</span>
-                  <span class="hash">#Maintenance</span>
-                  <span class="hash">#Seo</span>
-                </div>
-                <p class="service__wording">
-                  웹사이트 구축 이후 버그나 의도와 다르게 만들어진 부분은 수정해드리며, 유지보수와 사이트 운영 관련하여 문의하실 수 있습니다.
-                </p>
+          <li>
+            <div class="service__contents">
+              <div class="service__header">
+                <span class="number">07</span>
+                <h3 class="title">기타 서비스</h3>
+                <span class="hash">#Operation</span>
+                <span class="hash">#Maintenance</span>
+                <span class="hash">#Seo</span>
               </div>
+              <p class="service__wording">
+                웹사이트 구축 이후 버그나 의도와 다르게 만들어진 부분은 수정해드리며, 유지보수와 사이트 운영 관련하여 문의하실 수 있습니다.
+              </p>
             </div>
           </li>
         </ul>
